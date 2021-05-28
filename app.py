1f60974b318e64b0708be7222aaf39772e53adfd
gretaegan <greta.egan@gmail.com>
2021-05-28 11:44:51 +0000

    Connect app to MongoDB, add env.py file

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +10,19 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_definitions")
+def get_definitions():
+    definitions = mongo.db.definitions.find()
+    return render_template("definitions.html", definitions=definitions)
 
 
 if __name__ == "__main__":
