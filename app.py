a0a7d85333a542022182ebe28a76de3f09de1721
gretaegan <greta.egan@gmail.com>
2021-05-31 10:37:47 +0000

    Add basic Bootstrap Form and add register functionality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def get_definitions():
     definitions = mongo.db.definitions.find()
     return render_template("definitions.html", definitions=definitions)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
